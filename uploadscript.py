338815e3e6d930f33e10fca6482c6bf3e6bced93
amitej <v.amitej@gmail.com>
2018-02-28 00:27:04 -0500

    made changes to python script that pushed csv data to a database

--- uploadscript.py
+++ uploadscript.py
@@ -7,12 +7,12 @@ with open('data.csv', 'r') as fin:
     myList = [line for line in csvin]
 
 for sublist in myList[1:]:
-    data = {"Shelter Name": sublist[1],
-            "Capacity": sublist[2],
-            "Restrictions": sublist[3],
-            "Longitude": sublist[4],
-            "Latitude": sublist[5],
-            "Address": sublist[6],
-            "Special Notes": sublist[7],
-            "Phone Number": sublist[8]}
+    data = {"name": sublist[1],
+            "capacity": sublist[2],
+            "restrictions": sublist[3],
+            "longitude": sublist[4],
+            "latitude": sublist[5],
+            "address": sublist[6],
+            "specialNotes": sublist[7],
+            "phoneNumber": sublist[8]}
     firebase.patch("/Shelters/Shelter_Number_%02d" % (int(sublist[0]),), data)

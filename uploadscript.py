fca6f5c3e0418fc994d72b08d330e6b1e4d23fcc
Sidharth Potdar <sidharth.potdar@gmail.com>
2018-03-05 22:21:11 -0500

    Updated python script

--- uploadscript.py
+++ uploadscript.py
@@ -1,18 +1,37 @@
-from firebase import firebase
-import csv
+import firebase
+import pandas as pd
+
 firebase = firebase.FirebaseApplication("https://cs-2340-project.firebaseio.com")
 
-with open('data.csv', 'r') as fin:
-    csvin = csv.reader(fin)
-    myList = [line for line in csvin]
+# read in csv data
+df = pd.read_csv('data.csv')
 
-for sublist in myList[1:]:
-    data = {"name": sublist[1],
-            "capacity": sublist[2],
-            "restrictions": sublist[3],
-            "longitude": sublist[4],
-            "latitude": sublist[5],
-            "address": sublist[6],
-            "specialNotes": sublist[7],
-            "phoneNumber": sublist[8]}
-    firebase.patch("/Shelters/Shelter_Number_%02d" % (int(sublist[0]),), data)
+# edits data to add gender and age range columns
+genders = []
+ages = []
+
+for index, row in df.iterrows():
+    row_genders = ''
+    row_ages = []
+    if 'men' in row['Restrictions'].lower() and 'women' not in row['Restrictions'].lower():
+        row_genders = 'Men'
+    if 'women' in row['Restrictions'].lower():
+        row_genders = 'Women'
+    if 'children' in row['Restrictions'].lower():
+        row_ages.append('Children')
+    if 'young adults' in row['Restrictions'].lower():
+        row_ages.append('Young adults')
+    if 'families w/ newborns' in row['Restrictions'].lower():
+        row_ages.append('Families w/ newborns')
+    if 'anyone' in row['Restrictions'].lower():
+        row_ages.append('Anyone')
+        
+    genders.append('Any') if row_genders == '' else genders.append(row_genders)
+    ages.append(['Any']) if row_ages == [] else ages.append(row_ages)
+    
+df['Gender'] = genders
+df['Age Range'] = ages
+
+# send data to firebase
+data = df.drop(['Unique Key'], axis=1).to_json()
+firebase.patch("/Shelters/Shelter_Number_%02d" % (int(sublist[0]),), data)
